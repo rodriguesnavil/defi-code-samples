8eee7740fa2402f18f567965b9a895ad6c60ee20
Navil Rodrigues <rodriguesnavil@gmail.com>
2022-07-05 22:31:17 +0530

    Add folder structure

---
+++ hardhat.config.js
@@ -0,0 +1,64 @@
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-deploy")
+require("@nomiclabs/hardhat-etherscan")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("dotenv").config()
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
+const FORKING_BLOCK_NUMBER = process.env.FORKING_BLOCK_NUMBER
+const RINKEBY_RPC_URL =
+    process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alchemyapi.io/v2/your-api-key"
+const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "https://eth-kovan.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const REPORT_GAS = process.env.REPORT_GAS || false
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            // gasPrice: 130000000000,
+        },
+        kovan: {
+            url: KOVAN_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 42,
+            blockConfirmations: 6,
+        },
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
+        },
+    },
+    gasReporter: {
+        enabled: REPORT_GAS,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+    },
+    solidity: "0.8.7",
+    mocha: {
+        timeout: 300000, // wait for 300 seconds
+    },
+}
