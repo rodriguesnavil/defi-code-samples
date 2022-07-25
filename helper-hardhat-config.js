82fa3fe7625a60c1ec1a955d75c005d9c4c7065c
Navil Rodrigues <rodriguesnavil@gmail.com>
2022-07-26 00:47:49 +0530

    Update token and whale addresses

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -4,19 +4,19 @@ const networkConfig = {
     },
     31337: {
         name: "localhost",
-        daiEthPriceFeed: "0x773616E4d11A78F511299002da57A0a94577F1f4",
-        btcEthPriceFeed: "0xdeb288F737066589598e9214E782fa5A8eD689e8",
+        daiToken: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+        wethToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        daiWhale: "0xad0135af20fa82e106607257143d0060a7eb5cbf",
+        wethWhale: "0x06920c9fc643de77b99cb7670a944ad31eaaa260",
     },
     4: {
         name: "rinkeby",
-        daiEthPriceFeed: "0x74825DbC8BF76CC4e9494d0ecB210f676Efa001D",
-        wbtcEthPriceFeed: "0x2431452A0010a43878bF198e170F6319Af6d27F4",
         dai: "0xFab46E002BbF0b4509813474841E0716E6730136", // https://erc20faucet.com/
         wbtc: "0x577D296678535e4903D59A4C929B718e1D575e0A", // https://rinkeby.etherscan.io/token/0x577d296678535e4903d59a4c929b718e1d575e0a#writeContract
     },
 }
 
-const developmentChains = ["hardhat", "localhost"]
+const developmentChains = ["hardhat", "localhost", "rinkeby"]
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6
 
 module.exports = {
